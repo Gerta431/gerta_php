06d6532bb82d694dd8a6d0bde38ae0dea5a3bcfe
Gerta431 <musligerta08@gmail.com>
2024-11-20 17:41:03 +0100

    .

---
+++ module_9/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our php form</title>
+</head>
+<body>
+    <form action="">
+        <label for="username">Username: </label>
+        <input type="text" id="username" name="username" placeholder="username"><br><br>
+        <label for="password">Password: </label>
+        <input type="text" id="password" name="password" placeholder="password"><br><br>
+
+    <input type="submit" value="submit">
+    </form>
+</body>
+</html>
